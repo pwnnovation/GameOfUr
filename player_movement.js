53b23850c7e6141f81183225205dacc0fbe06e5b
Aaron Keller <aaron@connectio.us>
2017-07-15 15:36:08 -0700

    Added random generator to see which player goes first

---
+++ player_movement.js
@@ -0,0 +1,17 @@
+function firstplayer(){
+	var color = Math.floor(Math.random() * 2);
+
+	if (color === 0) {
+		console.log('Red Player Goes First');
+	}	
+	else {
+		console.log('Blue Player Goes First');
+	}
+
+}
+
+firstplayer();
+
+
+
+
